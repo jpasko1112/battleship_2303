7927e810dd1d9cbf30dd95fae8a30c1211532763
Justin Pasko <jpasko1112@gmail.com>
2023-05-25 20:34:45 -0400

    test: Add tests for health, sunk and hit.

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -2,20 +2,45 @@ require 'spec_helper'
 
 RSpec.describe Ship do
   before(:each) do
-    @ship = Ship.new('Cruiser', 3)
+    @cruiser = Ship.new('Cruiser', 3)
   end
 
   describe '#initialize' do
     it 'can initalize' do
-      expect(@ship).to be_a(Ship)
+      expect(@cruiser).to be_a(Ship)
     end
 
     it 'initializes with a name' do
-      expect(@ship.name).to eq('Cruiser')
+      expect(@cruiser.name).to eq('Cruiser')
     end
 
     it 'initializes with a length' do
-      expect(@ship.length).to eq(3)
+      expect(@cruiser.length).to eq(3)
+    end
+  end
+
+  describe '#health' do
+    it 'returns the ships health' do
+      expect(@cruiser.health).to eq(3)
+    end
+  end
+
+  describe '#sunk?' do
+    it 'returns true if the ship is sunk and defaults to false' do
+      expect(@cruiser.sunk?).to eq(false)
+    end
+  end
+
+  describe '#hit' do
+    it 'reduces health by 1' do
+      expect(@cruiser.health).to eq(3)
+      @cruiser.hit
+      expect(@cruiser.health).to eq(2)
+      @cruiser.hit
+      expect(@cruiser.health).to eq(1)
+      expect(@cruiser.sunk?).to eq(false)
+      @cruiser.hit
+      expect(@cruiser.sunk?).to eq(true)
     end
   end
 end
