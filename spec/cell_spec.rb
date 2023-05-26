44afe63c974e5279e4e5afe9d571dfadba96200c
Justin Pasko <jpasko1112@gmail.com>
2023-05-26 16:12:55 -0400

    test: Add test for render.

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -2,34 +2,35 @@ require 'spec_helper'
 
 RSpec.describe Cell do 
     before(:each) do 
-        @cell = Cell.new("B4")
+        @cell_1 = Cell.new("B4")
+        @cell_2 = Cell.new("C3")
         @cruiser = Ship.new("Cruiser", 3)
     end
 
     describe '#initialize' do
       it 'can initialize' do
-        expect(@cell).to be_a(Cell)
+        expect(@cell_1).to be_a(Cell)
       end 
     end
 
     describe '#coordinate' do
       it 'can coordinate cell location' do
-        expect(@cell.coordinate).to eq("B4")
+        expect(@cell_1.coordinate).to eq("B4")
       end   
     end  
 
     describe '#ship' do
       it 'can see if ship is in coordinate' do
-        expect(@cell.ship).to eq(nil)
+        expect(@cell_1.ship).to eq(nil)
       end
     end
 
     describe '#place_ship' do
       it 'places ship in cell' do
-        expect(@cell.ship).to eq(nil)
-        @cell.place_ship(@cruiser)
-        expect(@cell.ship).to eq(@cruiser)
-        expect(@cell.empty?).to eq(false)
+        expect(@cell_1.ship).to eq(nil)
+        @cell_1.place_ship(@cruiser)
+        expect(@cell_1.ship).to eq(@cruiser)
+        expect(@cell_1.empty?).to eq(false)
       end
     end
 
@@ -49,6 +50,23 @@ RSpec.describe Cell do
         expect(@cell.fired_upon?).to eq(true)
       end
     end
+
+    describe '#render' do
+      it 'returns a string representation of the cell' do
+        expect(@cell_1.render).to eq('.')
+        @cell_1.fire_upon
+        expect(@cell_1.render).to eq('M')
+        @cell_2.place_ship(@cruiser)
+        expect(@cell_2.render).to eq('.')
+        expect(@cell_2.render(true)).to eq('S')
+        @cell_2.fire_upon
+        expect(@cell_2.render).to eq('H')
+        expect(@cruiser.sunk?).to eq(false)
+        @cruiser.hit
+        @cruiser.hit
+        expect(@cruiser.sunk?).to eq(true)
+        expect(@cell_2.render).to eq('X')
+      end
+    end
 end
 
-# Create branch beforte beginning work
