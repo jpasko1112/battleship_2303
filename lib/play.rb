d6e4bc0236e77feb283c9eae9e0cfe2eaec4d8b1
Blake Sergesketter <blakesergesketter@gmail.com>
2023-05-29 17:19:23 -0500

    add user and computer sunk_ships attributes to Play class.

--- lib/play.rb
+++ lib/play.rb
@@ -1,6 +1,8 @@
 class Play 
-   attr_reader :menu  
+    attr_reader :menu, :user_sunk_ships, :comp_sunk_ships 
   def initialize 
     @menu = menu 
+    @user_sunk_ships = 0
+    @comp_sunk_ships = 0
   end
 end
