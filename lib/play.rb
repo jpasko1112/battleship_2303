51afa96057b39e801e81e4ba97f1b0d99a1f4698
Blake Sergesketter <blakesergesketter@gmail.com>
2023-05-31 07:10:43 -0500

    allign battleship art and add graphic header.

--- lib/play.rb
+++ lib/play.rb
@@ -15,8 +15,28 @@ class Play
   end
 
   def menu 
-    sleep(1.0)
-    puts "Welcome to BATTLESHIP!\n"
+puts "                                      |__
+                                      |\/
+                                      ---
+                                      /  | [
+                                      |  |||
+                              _/|     _/|-++'
+                          +  +--|    |--|--|_ |-
+                       { /|__|  |/\__|  |--- |||__/
+                       +---------------___[}-_===_.'____               /\    
+                  ____`-' ||___-{]_| _[}-  |     |_[___\==--          \/   _
+      __..._____--==/___]_|__|_____________________________[___\==--___,-----' .7
+     |                                                                   BB-61/
+      \_______________________________________________________________________|
+"
+    sleep(0.5)
+    puts "██╗    ██╗███████╗██╗      ██████╗ ██████╗ ███╗   ███╗███████╗        ████████╗ ██████╗         ██████╗  █████╗ ████████╗████████╗██╗     ███████╗███████╗██╗  ██╗██╗██████╗     ██╗    ██╗
+██║    ██║██╔════╝██║     ██╔════╝██╔═══██╗████╗ ████║██╔════╝        ╚══██╔══╝██╔═══██╗        ██╔══██╗██╔══██╗╚══██╔══╝╚══██╔══╝██║     ██╔════╝██╔════╝██║  ██║██║██╔══██╗    ██║    ██║
+██║ █╗ ██║█████╗  ██║     ██║     ██║   ██║██╔████╔██║█████╗             ██║   ██║   ██║        ██████╔╝███████║   ██║      ██║   ██║     █████╗  ███████╗███████║██║██████╔╝    ██║    ██║
+██║███╗██║██╔══╝  ██║     ██║     ██║   ██║██║╚██╔╝██║██╔══╝             ██║   ██║   ██║        ██╔══██╗██╔══██║   ██║      ██║   ██║     ██╔══╝  ╚════██║██╔══██║██║██╔═══╝     ╚═╝    ╚═╝
+╚███╔███╔╝███████╗███████╗╚██████╗╚██████╔╝██║ ╚═╝ ██║███████╗           ██║   ╚██████╔╝        ██████╔╝██║  ██║   ██║      ██║   ███████╗███████╗███████║██║  ██║██║██║         ██╗    ██╗
+ ╚══╝╚══╝ ╚══════╝╚══════╝ ╚═════╝ ╚═════╝ ╚═╝     ╚═╝╚══════╝           ╚═╝    ╚═════╝         ╚═════╝ ╚═╝  ╚═╝   ╚═╝      ╚═╝   ╚══════╝╚══════╝╚══════╝╚═╝  ╚═╝╚═╝╚═╝         ╚═╝    ╚═╝
+                                                                                                                                                                                          \n"
     sleep(1.0)
     puts "Who's reporting for duty? (ENTER NAME)"
     name = gets.chomp.capitalize
@@ -41,4 +61,52 @@ class Play
     end
   end
 
+  def comp_ship_placement 
+    comp_sub_placement 
+    comp_cruiser_placement 
+    puts @comp_board.render
+    puts "I have laid out my ships on the grid."
+    puts "You now need to lay out your two ships."
+    puts "The Cruiser is three units long and the Submarine is two units long."
+  end
+
+def user_cruiser_placement 
+  puts "Enter squares for your Cruiser (3 spaces):"
+  user_cruiser_input = gets.chomp.upcase.split 
+  if @user_board.valid_placement?(@user_cruiser, user_cruiser_input) == true 
+    @user_board.place(@user_cruiser, user_cruiser_input)
+  else
+    puts "Those are invalid coordinates. Please try again."
+    user_cruiser_placement 
+  end
+end 
+
+def user_sub_placement 
+  puts "Enter squares for your Submarine (2 spaces):"
+  user_sub_input = gets.chomp.upcase.split 
+  if @user_board.valid_placement?(@user_sub, user_sub_input) == true 
+    @user_board.place(@user_sub, user_sub_input)
+  else
+    puts "Those are invalid coordinates. Please try again."
+    user_sub_placement 
+  end
+end 
+
+# ----HELPER METHOD----
+
+  def comp_sub_placement
+    valid_sub_coordinates = @comp_board.cells.keys.sample(2)
+    until @comp_board.valid_placement?(@comp_sub, valid_sub_coordinates) == true 
+      valid_sub_coordinates = @comp_board.cells.keys.sample(2)
+    end
+    @comp_board.place(@comp_sub, valid_sub_coordinates)
+  end
+
+  def comp_cruiser_placement
+    valid_cruiser_coordinates = @comp_board.cells.keys.sample(3)
+    until @comp_board.valid_placement?(@comp_cruiser, valid_cruiser_coordinates) == true 
+      valid_cruiser_coordinates = @comp_board.cells.keys.sample(3)
+    end
+    @comp_board.place(@comp_cruiser, valid_cruiser_coordinates)
+  end
 end
--- runner.rb
+++ runner.rb
@@ -2,3 +2,6 @@ require './spec/spec_helper'
 
 play = Play.new
 play.menu 
+play.comp_ship_placement
+play.user_cruiser_placement
+play.user_sub_placement
